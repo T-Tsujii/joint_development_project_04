a2de1fdaace3268edcdab2e4548f6628967c8ee2
Kota Tsunodae <tsunoda.0688k@outlook.jp>
2020-04-07 22:07:47 +0900

    new,create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,23 @@
-class MessagesController < ApplicationControll
+class MessagesController < ApplicationController
+ #一覧画面に対するアクション
+  def index
+   @messages = Message.all
+  end
+ #新規作成に関するアクション
+  def new
+   @message = Message.new
+  end
+
+ # 以下のアクションを追加
+  def create
+    # Strong Parameter の記述
+    Message.create(message_params)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:title, :content)
+  end
 
 end
